03a1ebee1e04754a73b7a7649cdaddf53068daf1
totetero <totetero@gmail.com>
2021-08-28 07:51:04 +0900

    どーん

--- bin/getOpenCv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=$(cd $(dirname $0); pwd)/../dist/opencv
-mkdir -p $DIR
-curl https://docs.opencv.org/3.4.0/opencv.js > ${DIR}/opencv.js
-curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml > ${DIR}/haarcascade_eye.xml
-
